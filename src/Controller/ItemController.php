14d986f6eaa9888cf673f336c8cc7144d95243a8
Saidazim <134857217+saidazimisroil@users.noreply.github.com>
2024-05-26 18:49:43 +0500

    feat: add update, delete Item

--- src/Controller/ItemController.php
+++ src/Controller/ItemController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/item')]
 class ItemController extends AbstractController
 {
     public function __construct(
@@ -18,7 +19,7 @@ class ItemController extends AbstractController
         private EntityManagerInterface $em
     ) {}
 
-    #[Route('/item', name: 'app_item')]
+    #[Route('/', name: 'app_item')]
     public function index(): Response
     {
         return $this->render('item/index.html.twig', [
@@ -26,7 +27,7 @@ class ItemController extends AbstractController
         ]);
     }
 
-    #[Route('/item/new/{id<\d+>}', name: 'app_item_new', methods:['GET'])]
+    #[Route('/new/{id<\d+>}', name: 'app_item_new', methods:['GET'])]
     public function new(int $id): Response
     {
         $collection = $this->em->getRepository(ItemCollection::class)->find($id);
@@ -36,7 +37,7 @@ class ItemController extends AbstractController
         ]);
     }
     
-    #[Route('/item/new/{id<\d+>}', name: 'app_item_save', methods:['POST'])]
+    #[Route('/new/{id<\d+>}', name: 'app_item_save', methods:['POST'])]
     public function save(int $id, Request $request): Response
     {
         $collection = $this->em->getRepository(ItemCollection::class)->find($id);
@@ -57,4 +58,57 @@ class ItemController extends AbstractController
     
         return $this->redirectToRoute('app_collection', ['id' => $id]);
     }    
+
+    #[Route('/edit/{id<\d+>}', name: 'app_item_edit', methods:['GET'])]
+    public function edit(int $id): Response
+    {
+        $item = $this->em->getRepository(Item::class)->find($id);
+
+        $collection = $item->getItemCollection();
+        $currentUser = $this->security->getUser();
+        if($currentUser !== $collection->getUser()){
+            return $this->redirectToRoute('app_collections_my');
+        }
+        
+        return $this->render('item/update_item.html.twig', [
+            'item' => $item,
+            'collection' => $collection,
+        ]);
+    }
+
+    #[Route('/edit/{id<\d+>}', name: 'app_item_update', methods:['POST'])]
+    public function saveChanges(int $id, Request $request): Response
+    {
+        $item = $this->em->getRepository(Item::class)->find($id);
+
+        $collection = $item->getItemCollection();
+        $currentUser = $this->security->getUser();
+        if($currentUser !== $collection->getUser()){
+            return $this->redirectToRoute('app_collections_my');
+        }
+
+        $item->setName($request->request->get('name'));
+
+        $this->em->persist($item);
+        $this->em->flush();
+
+        return $this->redirectToRoute('app_collection', ['id' => $collection->getId()]);
+    }
+
+    #[Route('/delete/{id<\d+>}', name: 'app_item_delete', methods:['GET'])]
+    public function delete(int $id): Response
+    {
+        $item = $this->em->getRepository(Item::class)->find($id);
+
+        $collection = $item->getItemCollection();
+        $currentUser = $this->security->getUser();
+        if($currentUser !== $collection->getUser()){
+            return $this->redirectToRoute('app_collections_my');
+        }
+
+        $this->em->remove($item);
+        $this->em->flush();
+
+        return $this->redirectToRoute('app_collection', ['id' => $collection->getId()]);
+    }
 }
