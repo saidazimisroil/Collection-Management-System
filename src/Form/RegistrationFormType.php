1ed9f71c2b3f9e991ecdf6a75bcfea951af39e1a
Saidazim <134857217+saidazimisroil@users.noreply.github.com>
2024-06-10 12:45:02 +0500

    feat: add Agree Terms checkbox on registration form

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -1,9 +1,11 @@
 <?php
+// src/Form/RegistrationFormType.php
 
 namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -20,8 +22,6 @@ class RegistrationFormType extends AbstractType
             ->add('email')
             ->add('birthday')
             ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
@@ -31,8 +31,15 @@ class RegistrationFormType extends AbstractType
                     new Length([
                         'min' => 6,
                         'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
                         'max' => 4096,
+                    ]),
+                ],
+            ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'You should agree to our terms.',
                     ]),
                 ],
             ])
